d93d0d0c5faede002c8cebc579e56f1d7a0211d9
Marcos Vogado <marcoshcvogado@gmail.com>
2025-10-28 11:22:42 -0300

    add get{id}, get

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjetoDBZ.Data;
+using ProjetoDBZ.Models;
 
 namespace ProjetoDBZ.Controllers
 {
@@ -12,6 +14,36 @@ namespace ProjetoDBZ.Controllers
         public PersonagensController(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Personagem>>> GetPersonagens()
+        {
+            var personagens = await _appDbContext.DBZ.ToListAsync();
+
+            return Ok(personagens);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Personagem>> GetPersonagem(int id)
+        {
+            var personagem = await _appDbContext.DBZ.FindAsync(id);
+
+            if (personagem == null)
+            {
+                return NotFound("Personagem não encontrado!");
+            }
+
+            return Ok(personagem);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>AddPersonagem(Personagem personagem)
+        {
+            _appDbContext.DBZ.Add(personagem);
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(personagem);
         }
     }
 }
